08bfed010a0c81c6a2693e0f1ff30d14bfbe87a1
Jose Luis NF <84213931+iLuis116@users.noreply.github.com>
2024-02-28 19:18:59 -0600

    06 Uso de Use client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
-import Image from "next/image";
+
 import styles from "./page.module.css";
 
 export default function Home() {
+  console.log("Hola Mundo Pagina de inicio")
   return (
     <main className={styles.main}>
       <h1>Hola mundo</h1>
