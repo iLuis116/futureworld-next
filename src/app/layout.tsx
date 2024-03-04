6193e4d65068969495f5fd0861fa55bb6cad46e2
Jose Luis NF <84213931+iLuis116@users.noreply.github.com>
2024-03-03 18:39:23 -0600

    07 Arquitectura

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,23 @@
-"use client"
 
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
-import { Header } from "app/components/Header";
+import { Header } from "app/components/shared/Header";
+import { Footer } from "app/components/shared/Footer";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
-  console.log("Hola mundo layout")
+}) {
+
   return (
     <html lang="en">
-      <Header />
       <body className={inter.className}>
-        {children}</body>
+        <Header />
+        {children}
+        <Footer />
+        </body>
     </html>
   );
 }
