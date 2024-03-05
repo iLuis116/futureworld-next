ccde17c0fc460ca745740f223565990bf9256636
Jose Luis NF <84213931+iLuis116@users.noreply.github.com>
2024-03-04 20:01:44 -0600

    13 Optimización de componente image

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -4,11 +4,12 @@ import Image from "next/image"
 export const Description = () => {
     return (
         <section className = {styles.Description}>
+            <div className={styles.Description__imageContainer}>
             <Image src="/images/description.jpeg"
             alt="products marketplace"
-            width={500}
-            height={300}
+            fill
             />
+            </div>
             <div className={styles.Description__text}>
             <h2>Description</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero facilis doloribus, dolorum voluptatibus velit delectus! Maxime culpa ad sint, reprehenderit, dolor repudiandae asperiores a, exercitationem quas laudantium qui facere sit.</p>
