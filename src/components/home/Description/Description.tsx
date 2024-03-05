edcc65c86b6a73574fccc7b37237ea50e9ee4cae
Jose Luis NF <84213931+iLuis116@users.noreply.github.com>
2024-03-04 19:55:08 -0600

    12 Optimización de imagenes

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -1,9 +1,18 @@
+import styles from "./Description.module.sass"
+import Image from "next/image"
+
 export const Description = () => {
-    return(
-        <section>
-            <img src="/images/description.jpeg" alt="products marketplace" />
+    return (
+        <section className = {styles.Description}>
+            <Image src="/images/description.jpeg"
+            alt="products marketplace"
+            width={500}
+            height={300}
+            />
+            <div className={styles.Description__text}>
             <h2>Description</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero facilis doloribus, dolorum voluptatibus velit delectus! Maxime culpa ad sint, reprehenderit, dolor repudiandae asperiores a, exercitationem quas laudantium qui facere sit.</p>
+            </div>
         </section>
     )
 }
