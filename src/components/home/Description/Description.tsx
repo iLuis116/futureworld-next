1bd60269c86965eebffb1207af00944e290f5a16
Jose Luis NF <84213931+iLuis116@users.noreply.github.com>
2024-03-04 19:25:28 -0600

    11 Archivos estaticos

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -1,7 +1,9 @@
 export const Description = () => {
     return(
         <section>
-            <h1>Description</h1>
+            <img src="/images/description.jpeg" alt="products marketplace" />
+            <h2>Description</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero facilis doloribus, dolorum voluptatibus velit delectus! Maxime culpa ad sint, reprehenderit, dolor repudiandae asperiores a, exercitationem quas laudantium qui facere sit.</p>
         </section>
     )
 }
