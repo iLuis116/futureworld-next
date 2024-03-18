8c375d3e7e2b54632f21e6024167d2e379981679
Jose Luis NF <84213931+iLuis116@users.noreply.github.com>
2024-03-18 15:47:05 -0600

    17 Manejo de una API

--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -1,7 +1,19 @@
-'use client'
+import { json } from "stream/consumers"
 
-export const MainProducts = () => {
-    console.log("variable entorno",process.env.NEXT_PUBLIC_SHOPIFY_HOSTNAME)
+const getProducts = async () =>{
+    const response = await fetch(`${process.env.SHOPIFY_HOSTNAME}/admin/api/2023-10/products.json`, {
+        headers: new Headers({
+          'X-Shopify-Access-Token': process.env.SHOPIFY_API_KEY || ""
+        })
+      })
+
+      const data = await response.json()
+      return data
+}
+
+export const MainProducts = async () => {
+    const products = await getProducts()
+    console.log(products)
     return(
         <section>
             <h1>Main Products</h1>
